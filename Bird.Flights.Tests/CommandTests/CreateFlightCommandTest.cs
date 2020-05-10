a91f35d99f17040fa6deb64e799cf2a8521158c2
Davi Zucas <davi_zucas@hotmail.com>
2020-05-10 12:49:00 -0300

    implanted Flight command tests

---
+++ Bird.Flights.Tests/CommandTests/CreateFlightCommandTest.cs
@@ -0,0 +1,33 @@
+﻿  
+using System;
+using Bird.Flights.Domain.Commands;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bird.Flights.Tests.CommandTests
+{
+    [TestClass]
+    public class CreateFlightCommandTest
+    {
+        private readonly CreateFlightCommand _invalidFlight = new CreateFlightCommand("3000", "", "CGH", DateTime.Now.AddHours(-1), DateTime.Now, "A320");
+        private readonly CreateFlightCommand _validFlight = new CreateFlightCommand("3000", "CWB", "CGH", DateTime.Now.AddHours(-1), DateTime.Now, "A320");
+
+        public CreateFlightCommandTest()
+        {
+            _invalidFlight.Validate();
+            _validFlight.Validate();
+        }
+
+        [TestMethod]
+        public void Dado_um_voo_invalido()
+        {
+            Assert.AreEqual(_invalidFlight.Invalid, true);
+        }
+
+        [TestMethod]
+        public void Dado_um_voo_valido()
+        {
+            Assert.AreEqual(_validFlight.Valid, true);
+        }
+
+    }
+}
