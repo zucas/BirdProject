719bc707b2071ee9b479ae94c888199194521aa9
Davi Zucas <davi_zucas@hotmail.com>
2020-05-10 12:10:43 -0300

    Create FakeRepository to make tests

--- Bird.Flights.Tests/Repositories/FakeFlightRepository.cs
+++ Bird.Flights.Tests/Repositories/FakeFlightRepository.cs
@@ -1,12 +1,35 @@
-﻿using Bird.Flights.Domain;
+﻿using System;
+using System.Collections.Generic;
+using Bird.Flights.Domain.Entities;
+using Bird.Flights.Domain.Repositories;
 
 namespace Bird.Flights.Tests.Repositories
 {
     public class FakeFlightRepository : IFlightRepository
     {
-        public void Create(Flight flight)
+        void IFlightRepository.Create(Flight flight)
         {
+            throw new NotImplementedException();
+        }
 
+        IEnumerable<Flight> IFlightRepository.GetAll(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Flight IFlightRepository.GetById(Guid id)
+        {
+            return new Flight("3000", "CWB", "CGH", DateTime.Now.AddHours(-1), DateTime.Now, "A320");
+        }
+
+        IEnumerable<Flight> IFlightRepository.GetByPeriod(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IFlightRepository.Update(Flight flight)
+        {
+            
         }
     }
 }
